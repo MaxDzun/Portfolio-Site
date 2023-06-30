e42169d117c6d5dfb96d56e5111bd41e11e77e5f
Max Dzundza <maxcrafter23@gmail.com>
2023-06-30 12:57:19 +0300

    forma

--- feedback.php
+++ feedback.php
@@ -1,1 +1,23 @@
 <?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $budget = $_POST['budget'];
+    $message = $_POST['message'];
+
+    $to = "fugopannakota@gmail.com";
+    $date = date ("d.m.Y");
+    $time = date ("h:i");
+    $from = $email;
+    $subject = "Заявка з сайту";
+
+
+    $msg="
+        Имя: $name /n
+        Пошта: $email /n
+        Телефон: $phone /n
+        Бюджет: $phone /n
+        Текст: $message";
+    mail($to, $subject, $msg, "From: $to ");
+
+?>
